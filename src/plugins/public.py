8f6d987f4e47e91b605f4f42cedbbffbbfdb0f6b
YukkuriC <799433638@qq.com>
2023-09-03 20:41:36 +0800

    裁剪public plugin

--- src/plugins/public.py
+++ src/plugins/public.py
@@ -1,22 +1,13 @@
-from nonebot import on_command, on_notice
+from nonebot import on_command
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Message, Event, Bot, MessageSegment
-from nonebot.exception import IgnoredException
-from nonebot.message import event_preprocessor
 from src.libraries.image import *
 
-
-@event_preprocessor
-async def preprocessor(bot, event, state):
-    if hasattr(event, 'message_type') and event.message_type == "private" and event.sub_type != "friend":
-        raise IgnoredException("not reply group temp message")
-
-        
 help = on_command('help')
 
 
 @help.handle()
-async def _(bot: Bot, event: Event, state: T_State):
+async def _(bot: Bot, event: Event, state: T_State, message:Message):
     help_str = '''可用命令如下：
 今日舞萌 查看今天的舞萌运势
 XXXmaimaiXXX什么 随机一首歌
@@ -32,23 +23,3 @@ XXXmaimaiXXX什么 随机一首歌
             "file": f"base64://{str(image_to_base64(text_to_image(help_str)), encoding='utf-8')}"
         })
     ]))
-
-
-async def _group_poke(bot: Bot, event: Event) -> bool:
-    value = (event.notice_type == "notify" and event.sub_type == "poke" and event.target_id == int(bot.self_id))
-    return value
-
-
-poke = on_notice(rule=_group_poke, priority=10, block=True)
-
-
-@poke.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    if event.__getattribute__('group_id') is None:
-        event.__delattr__('group_id')
-    await poke.send(Message([
-        MessageSegment("poke",  {
-           "qq": f"{event.sender_id}"
-       })
-    ]))
-
