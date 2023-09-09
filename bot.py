9d4cb2faba20394e90173280692138061e8e10e8
YukkuriC <799433638@qq.com>
2023-09-09 16:48:29 +0800

    更彻底的夺舍

--- bot.py
+++ bot.py
@@ -1,37 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from collections import defaultdict
+from src import inject_cmdbot as nonebot
 
-import nonebot
-from nonebot.adapters.onebot.v11 import Adapter
-
-
-# Custom your logger
-# 
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
-
-# You can pass some keyword args config to init function
-nonebot.init()
-# nonebot.load_builtin_plugins()
-app = nonebot.get_asgi()
-
-driver = nonebot.get_driver()
-driver.register_adapter(Adapter)
-driver.config.help_text = {}
-
-
+nonebot.inject()
 nonebot.load_plugins("src/plugins")
-
-# Modify some config / config depends on loaded configs
-# 
-# config = driver.config
-# do something...
-
 
 if __name__ == "__main__":
     nonebot.run()
--- cmdbot.py
+++
@@ -1,7 +0,0 @@
-from src import inject_cmdbot as nonebot
-
-nonebot.inject()
-nonebot.load_plugins("src/plugins")
-
-if __name__ == "__main__":
-    nonebot.run()
